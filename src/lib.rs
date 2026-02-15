bffea02ff3fab924989fb1221b5859d863bbf676
Sidney Cammeresi <sac@cheesecake.org>
2026-02-14 20:19:41 -0800

    switch from paste to pastey

--- src/lib.rs
+++ src/lib.rs
@@ -97,7 +97,7 @@
 //! [FreeBSD]: https://github.com/freebsd/freebsd-src/blob/main/sys/sys/linker_set.h
 
 pub use linker_set_proc::set_entry;
-pub use paste::paste;
+pub use pastey::paste;
 
 /// An iterator that yields the elements in a linker set.
 pub struct LinkerSetIter<T> {
